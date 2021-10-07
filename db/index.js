905dfbb73e815e8c15697e1c40133ff9c32334c1
Sam Pool <sambpool@gmail.com>
2021-10-06 20:49:11 -0600

    edit db config

--- db/index.js
+++ db/index.js
@@ -6,9 +6,9 @@ const { dbConfig } = require('./config.js');
 
 // DB Connection
 const pool = new Pool(dbConfig);
-
+/*
 pool.connect()
 .then( console.log('\nConnected to PG!', '🐘🥜') )
 .catch( err => console.log( err ));
-
+*/
 module.exports.pool = pool;
--- utils/k6/stylesByProduct.js
+++ utils/k6/stylesByProduct.js
@@ -1,11 +1,11 @@
 const http = require('k6/http');
-const {sleep} = require('utils/k6/products');
+const {sleep} = require('k6');
 
 export const options = {
-  vus: 250,
+  vus: 100,
   duration: '10s'
 };
 export default function () {
-  http.get('http://127.0.0.1:3000/products');
+  http.get('http://54.176.172.64/products/5/styles');
   sleep(1);
 }
