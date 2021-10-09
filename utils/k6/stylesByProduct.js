66ba76322b474d6298924ca5f09043ffb0cdf012
Sam Pool <sambpool@gmail.com>
2021-10-08 18:04:09 -0600

    docker config

--- utils/k6/stylesByProduct.js
+++ utils/k6/stylesByProduct.js
@@ -2,10 +2,11 @@ const http = require('k6/http');
 const {sleep} = require('k6');
 
 export const options = {
-  vus: 100,
+  vus: 250,
   duration: '10s'
 };
 export default function () {
-  http.get('http://54.176.172.64/products/5/styles');
+  let productId = Math.floor(Math.random() * 1000000);
+  http.get(`http://ec2-54-153-69-192.us-west-1.compute.amazonaws.com/products/${productId}/styles`);
   sleep(1);
 }
