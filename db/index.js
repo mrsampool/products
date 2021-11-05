8f9eaa17dc209d0d870b57cbd44c9489796a2438
Sam Pool <64799470+mrsampool@users.noreply.github.com>
2021-11-05 04:28:59 -0600

    remove logs for production

--- db/index.js
+++ db/index.js
@@ -6,9 +6,5 @@ const { dbConfig } = require('./config.js');
 
 // DB Connection
 const pool = new Pool(dbConfig);
-/*
-pool.connect()
-.then( console.log('\nConnected to PG!', '🐘🥜') )
-.catch( err => console.log( err ));
-*/
+
 module.exports.pool = pool;
