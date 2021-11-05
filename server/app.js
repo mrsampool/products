c7e4215a3af98a1dbbc05856ad2ec4a791ecf314
Sam Pool <64799470+mrsampool@users.noreply.github.com>
2021-11-05 04:19:41 -0600

    remove logs for production

--- server/app.js
+++ server/app.js
@@ -2,15 +2,8 @@ const express = require('express');
 const app = express();
 const {router} = require('./router');
 const path = require('path');
-/*
-app.use( (req, res, next)=>{
-  console.log(`\nIncoming ${req.method} request to ${req.path}`);
-  console.log(req.body);
-  next();
-})
-*/
 
 app.use(express.static( path.join(__dirname, '..', 'static') ));
-app.use( router);
+app.use(router);
 
 module.exports.app = app;
